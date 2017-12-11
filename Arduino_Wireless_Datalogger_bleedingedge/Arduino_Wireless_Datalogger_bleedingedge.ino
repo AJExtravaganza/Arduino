33f3fa0ca7aa7160d495edb275d45ed507c578a8
Alex Dunn <alexdunnmobile@gmail.com>
2017-12-10 17:14:38 -0800

    set 15min DEADMANPERIOD

--- Arduino_Wireless_Datalogger_bleedingedge/Arduino_Wireless_Datalogger_bleedingedge.ino
+++ Arduino_Wireless_Datalogger_bleedingedge/Arduino_Wireless_Datalogger_bleedingedge.ino
@@ -24,7 +24,7 @@
 bool deviceWasDown = true; //debug variable for device failure
 
 const unsigned int SATELLITES = 1;
-const unsigned long int DEADMANPERIOD = 1000UL * 30UL;// DEBUG VALUE 60UL * 15UL; // Check in once every fifteen minutes
+const unsigned long int DEADMANPERIOD = 1000UL *  60UL * 15UL; // Check in once every fifteen minutes
 const unsigned long int SATELLITELOOPPERIOD = DEADMANPERIOD / 30UL; // Must be <10% of DEADMANPERIOD (for 0.90 threshold) and <=SATELLITEPOLLPERIOD
 const unsigned long int SATELLITEPOLLPERIOD = DEADMANPERIOD / 10UL; //Satellite poll rate (5sec for debugging)
 bool liveDevices[SATELLITES + 1]; //Index corresponds with device ID
