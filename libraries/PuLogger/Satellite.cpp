28d9964589001917820d9f5ee524b687e0f558ec
Alex Dunn <alexdunnmobile@gmail.com>
2017-12-12 19:47:04 -0800

    Fleshed out Satellite class in preparation for alarms and migration of core functionality to Satellite from the main source file

--- libraries/PuLogger/Satellite.cpp
+++ libraries/PuLogger/Satellite.cpp
@@ -1,10 +1,23 @@
 #include "Satellite.h"
 
-Satellite::Satellite() : deviceID(-1), deviceUp(false), tempRawValue(-1), humRawValue(-1), tempHighAlarm(false), tempLowAlarm(false), humHighAlarm(false), humLowAlarm(false) {
+Satellite::Satellite() : deviceID(-1), deviceUp(false), lastTransmission(0), 
+	tempRawValue(-1), humRawValue(-1), 
+	tempHighLimit(310), tempLowLimit(240), tempHighAlarm(false), tempLowAlarm(false), 
+	tempAlarmGracePeriod(1000UL * 60UL * 60UL),
+	humHighLimit(750), humLowLimit(600), humHighAlarm(false), humLowAlarm(false), 
+	humAlarmGracePeriod(1000UL * 60UL * 60UL) 
+{
 	
 }
 
-Satellite::Satellite(int deviceID) : deviceID(deviceID), deviceUp(false), tempRawValue(-1), humRawValue(-1), tempHighAlarm(false), tempLowAlarm(false), humHighAlarm(false), humLowAlarm(false) {
+Satellite::Satellite(int deviceID, unsigned long int tGrace, int tHigh, int tLow, unsigned long int hGrace, int hHigh, int hLow) : 
+	deviceID(deviceID), deviceUp(false), lastTransmission(0), 
+	tempRawValue(-1), humRawValue(-1), 
+	tempHighLimit(310), tempLowLimit(240), tempHighAlarm(false), tempLowAlarm(false), 
+	tempAlarmGracePeriod(1000UL * 60UL * 60UL),
+	humHighLimit(750), humLowLimit(600), humHighAlarm(false), humLowAlarm(false), 
+	humAlarmGracePeriod(1000UL * 60UL * 60UL) 
+{
 	
 }
 
--- libraries/PuLogger/Satellite.h
+++ libraries/PuLogger/Satellite.h
@@ -7,17 +7,25 @@ private:
 	
 public:
 	Satellite();
-	Satellite(int deviceID);
+	Satellite(int deviceID, unsigned long int tGrace, int tHigh, int tLow, unsigned long int hGrace, int hHigh, int hLow);
   
 	bool deviceUp;
+	unsigned long int lastTransmission;
 	
 	int tempRawValue; //Raw values in deci-units
   int humRawValue;
 	
+	int tempHighLimit; // Temperature alarm properties
+	int tempLowLimit;
 	bool tempHighAlarm;
 	bool tempLowAlarm;
+	unsigned long int tempAlarmGracePeriod;
+	
+	int humHighLimit; // Humidity alarm properties
+	int humLowLimit;
 	bool humHighAlarm;
 	bool humLowAlarm;
+	unsigned long int humAlarmGracePeriod;
 	
   float getTemp();
   float getHum();
