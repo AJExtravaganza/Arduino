255079ba39ef6e35f37b0b45f2ce9a8cb2db1247
Alex Dunn <alexdunnmobile@gmail.com>
2018-02-08 23:41:02 -0800

    Heat and fan control implemented.  Heat/Fan setpoints adjusted for current pumidor.  Consider this v1.0

--- libraries/PuLogger/Satellite.h
+++ libraries/PuLogger/Satellite.h
@@ -2,12 +2,14 @@
 #define SATELLITE
 
 	//// Alarm thresholds are defined here
-const int TEMPHIGHLIMIT = 315;
-const int TEMPLOWLIMIT = 270;
-const int HUMHIGHLIMIT = 750;
-const int HUMLOWLIMIT = 600;
-const unsigned long int TEMPALARMGRACEPERIOD = (1000UL *  60UL * 60UL); //1 hour
-const unsigned long int HUMALARMGRACEPERIOD = (1000UL * 60UL * 60UL * 3); //3 hours
+const int TEMPSP = 315;
+const int HUMSP = 700;
+const int TEMPHIGHLIMIT = TEMPSP;
+const int TEMPLOWLIMIT = TEMPSP - 25;
+const int HUMHIGHLIMIT = HUMSP + 50;
+const int HUMLOWLIMIT = HUMSP - 50;
+const unsigned long int TEMPALARMGRACEPERIOD = (1000UL *  60UL * 60UL * 5); //5 hours (4.5hrs recovery measured from 24C, which should be far below worst-case in normal use)
+const unsigned long int HUMALARMGRACEPERIOD = (1000UL * 60UL * 60UL * 2); //2 hours (1hr recovery measured, with 100% tolerance)
 
 
 class Satellite { //privatise whatever when you have a final architecture figured out.
--- libraries/PuLogger/mechElec.h
+++ libraries/PuLogger/mechElec.h
@@ -4,9 +4,13 @@
 #include <Arduino.h>
 
 const int BUZZERPIN = 2;
-const int FANPIN = 4;
+const int FANPIN = 5;
+const int HEATPIN = 4;
 
 void startFan();
 void stopFan();
+
+void startHeat();
+void stopHeat();
 
 #endif
